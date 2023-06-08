1847096aacaabb42367eefc0cf09c1d84f907c7a
DMcP89 <davemcpherson@wochstudios.com>
2023-06-07 22:54:06 -0400

    implementing weather widget

--- cmd/tinycare-tui/main.go
+++ cmd/tinycare-tui/main.go
@@ -1,46 +1,61 @@
 package main
 
 import (
-    "github.com/rivo/tview"
-    "github.com/DMcP89/tinycare-tui/internal/utils"
+	"github.com/DMcP89/tinycare-tui/internal/utils"
+	"github.com/rivo/tview"
 )
 
 func main() {
-   app := tview.NewApplication()
+	app := tview.NewApplication()
 
-   daily_commits, err := utils.GetDailyCommits("/home/dave/workspace/projects")
+	daily_commits, err := utils.GetDailyCommits("/home/dave/workspace/projects")
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-   dailyView := tview.NewTextView().
-        SetText(daily_commits).
-        SetTextAlign(tview.AlignLeft).
-        SetDynamicColors(true)
+	dailyView := tview.NewTextView().
+		SetText(daily_commits).
+		SetTextAlign(tview.AlignLeft).
+		SetDynamicColors(true)
 
-   dailyView.SetBorder(true).SetTitle("Daily Commits")
+	dailyView.SetBorder(true).SetTitle("Daily Commits")
 
-   weekly_commits, err := utils.GetWeeklyCommits("/home/dave/workspace/projects")
-   if err != nil {
-        panic(err)
-   }
+	weekly_commits, err := utils.GetWeeklyCommits("/home/dave/workspace/projects")
+	if err != nil {
+		panic(err)
+	}
 
-   weeklyView := tview.NewTextView().
-        SetText(weekly_commits).
-        SetTextAlign(tview.AlignLeft).
-        SetDynamicColors(true)
+	weeklyView := tview.NewTextView().
+		SetText(weekly_commits).
+		SetTextAlign(tview.AlignLeft).
+		SetDynamicColors(true)
 
-   weeklyView.SetBorder(true).SetTitle("Weekly Commits")
+	weeklyView.SetBorder(true).SetTitle("Weekly Commits")
 
-   flex := tview.NewFlex().
-            AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-            AddItem(dailyView,0,1,false).
-            AddItem(weeklyView,0,1,false),0,1,false)
+	weather, err := utils.GetWeather("07070")
+	if err != nil {
+		panic(err)
+	}
+	weatherView := tview.NewTextView().
+		SetText(weather).
+		SetTextAlign(tview.AlignCenter).
+		SetDynamicColors(true)
 
-   app.SetRoot(flex, true).SetFocus(flex)
+	weatherView.SetBorder(true).SetTitle("Weather")
 
-   if err := app.Run(); err != nil{
-        panic(err)
-   }
+	flex := tview.NewFlex().
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(dailyView, 0, 1, false).
+			AddItem(weeklyView, 0, 1, false), 0, 2, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(weatherView, 0, 1, false).
+			AddItem(tview.NewBox().SetBorder(true), 0, 1, false).
+			AddItem(tview.NewBox().SetBorder(true), 0, 3, false), 0, 1, false)
+
+	app.SetRoot(flex, true).SetFocus(flex)
+
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
 }
---
+++ internal/utils/weather.go
@@ -0,0 +1,39 @@
+package utils
+
+// Create a go function that will retrieve the current weather for a given postal code via web scraping.
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// GetWeather will retrieve the current weather for a given postal code via web scraping.
+func GetWeather(postalCode string) (string, error) {
+	// Make HTTP request
+	reqUrl := "https://weather.com/weather/today/l/" + postalCode + ":4:US"
+	fmt.Println(reqUrl)
+	response, err := http.Get(reqUrl)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	// Create a goquery document from the HTTP response
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// Get the weather
+	// The weather is not contained in a div called "today_nowcard-phrase" anymore
+	// the current temp is stored in a span with class CurrentConditions--tempValue--MHmYY
+	weather := document.Find("span.CurrentConditions--tempValue--MHmYY").First().Text()
+	location := document.Find("h1.CurrentConditions--location--1YWj_").First().Text()
+	weather = strings.TrimSpace(weather)
+
+	// Print the weather
+	return fmt.Sprintf("The weather in %s is %s", location, weather), nil
+}
