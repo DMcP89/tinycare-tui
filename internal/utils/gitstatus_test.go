ee60040bd7a0d06413b03cefe42ea9af9f9e3d33
DMcP89 <davemcpherson@wochstudios.com>
2024-04-15 13:20:28 -0400

    adding test files for remaining utils

---
+++ internal/utils/gitstatus_test.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	t.Fatalf("Test not implemented")
+}
---
+++ internal/utils/todoist_test.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestGetLocalTasks(t *testing.T) {
+	t.Fatalf("Test not implemented")
+}
+
+func TestGetTodaysTasks(t *testing.T) {
+	t.Fatalf("Test not implemented")
+}
+
+func TestGetCompletedTasks(t *testing.T) {
+	t.Fatalf("Test not implemented")
+}
